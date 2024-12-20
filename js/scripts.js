0e3a46f537321f35208846af5a0f21cad73be8e5
evelynpaigelee <evelynpaigelee@gmail.com>
2024-12-19 22:44:56 -0800

    Implementing JQuery for Hamburger Mobile Bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-alert("Welcome to Eve's Portfolio Website!");
+// alert("Welcome to Eve's Portfolio Website!");
+//
+//
+// console.log("Thank you for visiting Eve's Portfolio Website :)");
+//
 
-
-console.log("Thank you for visiting Eve's Portfolio Website :)");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
