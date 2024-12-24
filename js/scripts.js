9eb94870307cc3e47169a6c84d8256546b7aa1db
evelynpaigelee <evelynpaigelee@gmail.com>
2024-12-23 19:43:59 -0800

    StandardJS Checker

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,10 @@
 //
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-    } else {
-        x.className = 'navtoggle';
-    }
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
 }
